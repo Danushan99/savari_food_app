939321aac3c6c9a4a69abb0d717d752f799cac05
danushan99 <danuthasan08@gmail.com>
2021-10-07 13:44:05 +0530

    java for home page

--- app/src/main/java/mad/example/jeyavarman/home_page.java
+++ app/src/main/java/mad/example/jeyavarman/home_page.java
@@ -1,6 +1,7 @@
 package mad.example.jeyavarman;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -8,25 +9,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class home_page extends AppCompatActivity {
-public ImageButton imgprofile;
+public class home_page extends AppCompatActivity  implements View.OnClickListener {
+    public ImageButton imgprofile;
+    public CardView card1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        card1 = findViewById(R.id.tastpie);
+        card1.setOnClickListener(this);
+
+
+
+
         imgprofile = findViewById(R.id.imgprofile);
         imgprofile.setOnClickListener(new View.OnClickListener() {
-                                     @Override
-                                     public void onClick(View v) {
-                                         Intent intent = new Intent(home_page.this, profile.class);
-                                         startActivity(intent);
-                                         finish();
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home_page.this, profile.class);
+                startActivity(intent);
 
-                                     }
-                                 }
+
+
+            }
+        }
 
         );
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tastpie:
+                startActivity(new Intent(this, Customer.class));
+                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
 }
