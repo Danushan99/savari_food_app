f9a20a57188b24a09ecaca4fc188c12b2f27fd9b
danushan99 <danuthasan08@gmail.com>
2021-10-07 19:31:58 +0530

    payment done

--- app/src/main/java/mad/example/jeyavarman/done.java
+++ app/src/main/java/mad/example/jeyavarman/done.java
@@ -2,13 +2,30 @@ package mad.example.jeyavarman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class done extends AppCompatActivity {
+    private Button backhome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
+        backhome=findViewById(R.id.homebtn);
+        backhome.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(done.this,home_page.class);
+                startActivity(intent);
+
+            }
+
+
+
+        });
     }
 }
